a8b70372e6fe1834642c813420434721040985b1
Lucas Soares <lucassoares1793@gmail.com>
2018-04-01 23:59:09 -0300

    add signup form with fade effect

--- altHome/js/login.js
+++ altHome/js/login.js
@@ -1,7 +1,32 @@
-function login(username, password){
 
+var Groups = {
+	Anonymous: 1,
+	Normal: 2,
+	Admin: 3,
+};
+
+var modalBox = document.getElementById("signup_form");
+var userGroup = Groups.Anonymous;
+
+function login(username, password){
+	if(username == "admin" && password == "admin")
+		userGroup = Groups.Admin;
+	else userGroup = Groups.Normal;
+
+	location.reload(); 
 }
 
 function signup_form(){
+	modalBox.style.display = 'block';
+}
 
+function cancel_signup(){
+	modalBox.style.display = 'none'
+}
+
+// When the user clicks anywhere outside of the modal content, close it
+window.onclick = function(event) {
+    if (event.target == modalBox) {
+        modalBox.style.display = "none";
+    }
 }
