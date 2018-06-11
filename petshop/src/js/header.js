0d44c32dd5d376dc5e6d6e64a751e969c4e73ffc
Lucas Soares <lucassoares1793@gmail.com>
2018-06-11 00:14:37 -0300

    Change password template

--- petshop/src/js/header.js
+++ petshop/src/js/header.js
@@ -15,228 +15,228 @@ import logo from '../resources/logo.png';
 import {Link, NavLink} from 'react-router-dom'
 
 class ResponsiveLogo extends Component {
-	render() {
-		return(
-			<NavLink to='/'> <img id='logo' src={logo} alt='Loading' className='responsiveLogo'/> </NavLink>
-		);
-	}
+  render() {
+    return(
+      <NavLink to='/'> <img id='logo' src={logo} alt='Loading' className='responsiveLogo'/> </NavLink>
+    );
+  }
 }
 
 
 class UserInfo extends Component {
 
-	render () { 
-		return (
-			<Row className={'topHeader valign-wrapper'} style={{float: 'right'}}>
-				<Col> <b>Usuário:</b> {this.props.user.name}</Col>
-				<Col><img className='circle' src={this.props.user.image} style={{height: '55px'}}/> </Col>
-				<Col><Link to='/carrinho'><Button waves='light' className='btn'>Meu Carrinho<Icon left>shopping_cart</Icon></Button></Link></Col>
-				<Col><Link to='/perfil'><Button waves='light' className='btn'>Meu Perfil<Icon left>account_circle</Icon></Button></Link></Col>
-				<Col><Button waves='light' className='btn' onClick={ () => {this.props.handleLogin(null, null, true)} }>Sair<Icon left>exit_to_app</Icon></Button></Col>
-			</Row>
-		);
-	}
+  render () { 
+    return (
+      <Row className={'topHeader valign-wrapper'} style={{float: 'right'}}>
+        <Col> <b>Usuário:</b> {this.props.user.name}</Col>
+        <Col><img className='circle' src={this.props.user.image} style={{height: '55px'}}/> </Col>
+        <Col><Link to='/carrinho'><Button waves='light' className='btn'>Meu Carrinho<Icon left>shopping_cart</Icon></Button></Link></Col>
+        <Col><Link to='/perfil'><Button waves='light' className='btn'>Meu Perfil<Icon left>account_circle</Icon></Button></Link></Col>
+        <Col><Button waves='light' className='btn' onClick={ () => {this.props.handleLogin(null, null, true)} }>Sair<Icon left>exit_to_app</Icon></Button></Col>
+      </Row>
+    );
+  }
 }
 
 class LoginForm extends Component {
-	constructor(props) {
-		super(props)
-		this.props = {
-			handleLogin: props.handleLogin
-		}
+  constructor(props) {
+    super(props)
+    this.props = {
+      handleLogin: props.handleLogin
+    }
 
-		this.state = {
-			username: '',
-			password: ''
-		}
-	}
-
-	// Function to check if inputs are correct
-	validate = (e) => {
-		return true
-	}
-
-	// Update props value
-	handleChange = (e, stateToChange) => {
-		let newState = {}
-		newState[stateToChange] = e.target.value
-	  this.setState(newState)
+    this.state = {
+      username: '',
+      password: ''
+    }
   }
 
-	// Used primarily for ENTER press submission
+  // Function to check if inputs are correct
+  validate = (e) => {
+    return true
+  }
+
+  // Update props value
+  handleChange = (e, stateToChange) => {
+    let newState = {}
+    newState[stateToChange] = e.target.value
+    this.setState(newState)
+  }
+
+  // Used primarily for ENTER press submission
   handleKeyPress = (e) => {
 
     if(e.key === 'Enter') {
       if(this.validate(e)) {
-      	this.submit(e)
+        this.submit(e)
       }
     }
   }
 
   submit = (e) => {
-  	if(this.validate(e)) 
-  		this.props.handleLogin(this.state.username, this.state.password, false)
+    if(this.validate(e)) 
+      this.props.handleLogin(this.state.username, this.state.password, false)
   }
  
-	render () { 
-		return (
-			<Row className={'topHeader valign-wrapper'} style={{float: 'right'}}>
-				
-				<Col>
-					<Input className='input box-shadow' 
-								label='Usuário' 
-								validate 
-								type='text' 
-								onChange={ e => {this.handleChange(e, 'username')} }
-								onKeyDown={this.handleKeyPress}/>
-				</Col>
-				
-				<Col>
-					<Input className='input box-shadow' 
-								label='Senha' 
-								validate 
-								type='password' 
-								onChange={ e => {this.handleChange(e, 'password')} }
-								onKeyDown={this.handleKeyPress}/>
-				</Col>
+  render () { 
+    return (
+      <Row className={'topHeader valign-wrapper'} style={{float: 'right'}}>
+        
+        <Col>
+          <Input className='input box-shadow' 
+                label='Usuário' 
+                validate 
+                type='text' 
+                onChange={ e => {this.handleChange(e, 'username')} }
+                onKeyDown={this.handleKeyPress}/>
+        </Col>
+        
+        <Col>
+          <Input className='input box-shadow' 
+                label='Senha' 
+                validate 
+                type='password' 
+                onChange={ e => {this.handleChange(e, 'password')} }
+                onKeyDown={this.handleKeyPress}/>
+        </Col>
 
-				<Col>
-					<Button waves='light' 
-									className='btn' 
-									onClick={	(e) => {
-											this.props.handleLogin(this.state.username, this.state.password, false)
-										} 
-									}> Entrar </Button>
-				</Col>
+        <Col>
+          <Button waves='light' 
+                  className='btn' 
+                  onClick={ (e) => {
+                      this.props.handleLogin(this.state.username, this.state.password, false)
+                    } 
+                  }> Entrar </Button>
+        </Col>
 
-				<Col l={3}><Input className='input box-shadow' label='Lembrar de mim'type='checkbox'/></Col>
-			</Row>
-		);
-	}
+        <Col l={3}><Input className='input box-shadow' label='Lembrar de mim'type='checkbox'/></Col>
+      </Row>
+    );
+  }
 }
 
 
 class TopHeader extends Component {
 
-	constructor(props) {
-		super(props)
-		this.props = {
-			user: props.user,
-			handleLogin: props.handleLogin
-		};
-	}
+  constructor(props) {
+    super(props)
+    this.props = {
+      user: props.user,
+      handleLogin: props.handleLogin
+    };
+  }
 
-	render() {
-		if(this.props.user){
-			return(	
-				<Row className={'topHeader valign-wrapper hide-on-med-and-down'} style={{topMargin: '10px'}}>
-					{/*Horário de Funcionamento*/}
-					<Col l={3} m={3} s={3}><Icon tiny className='fa icon'>access_time</Icon> Aberto - Seg - Sex: 9am à 5pm</Col>
-					{/*User info*/}
-					<Col l={9} m={9} s={9} >
-						{/**/}
-						<UserInfo user={this.props.user} handleLogin={this.props.handleLogin}/>
-					</Col>
-				</Row>
-			);
-		} else {
-			return(	
-				<Row className={'topHeader valign-wrapper hide-on-med-and-down'} style={{topMargin: '10px'}}>
-					{/*Horário de Funcionamento*/}
-					<Col l={3} m={3} s={3}><Icon tiny className='fa icon'>access_time</Icon> Aberto - Seg - Sex: 9am à 5pm</Col>
-					{/*Login*/}
-					<Col l={8} m={8} s={8} offset='l1' >
-						{/*password e username*/}
-						<LoginForm handleLogin={this.props.handleLogin}/>
-					</Col>
-				</Row>
-			);
-		}
-	}
+  render() {
+    if(this.props.user){
+      return( 
+        <Row className={'topHeader valign-wrapper hide-on-med-and-down'} style={{topMargin: '10px'}}>
+          {/*Horário de Funcionamento*/}
+          <Col l={3} m={3} s={3}><Icon tiny className='fa icon'>access_time</Icon> Aberto - Seg - Sex: 9am à 5pm</Col>
+          {/*User info*/}
+          <Col l={9} m={9} s={9} >
+            {/**/}
+            <UserInfo user={this.props.user} handleLogin={this.props.handleLogin}/>
+          </Col>
+        </Row>
+      );
+    } else {
+      return( 
+        <Row className={'topHeader valign-wrapper hide-on-med-and-down'} style={{topMargin: '10px'}}>
+          {/*Horário de Funcionamento*/}
+          <Col l={3} m={3} s={3}><Icon tiny className='fa icon'>access_time</Icon> Aberto - Seg - Sex: 9am à 5pm</Col>
+          {/*Login*/}
+          <Col l={8} m={8} s={8} offset='l1' >
+            {/*password e username*/}
+            <LoginForm handleLogin={this.props.handleLogin}/>
+          </Col>
+        </Row>
+      );
+    }
+  }
 }
 
 
 class TopNavbar extends Component {
 
-	constructor(props) {
-		super(props)
-		this.props = {
-			user: props.user,
-			handleLogin: props.handleLogin
-		};
-	}
+  constructor(props) {
+    super(props)
+    this.props = {
+      user: props.user,
+      handleLogin: props.handleLogin
+    };
+  }
 
-	render() {
-		if(this.props.user && this.props.user.username == 'admin'){
-			return(
-				<div>
-					<Navbar className='navbar' brand={<ResponsiveLogo/>} right>
-						<SideNavItem className='hide-on-large-only' userView user={this.props.user}/>
-						<li><NavLink to='/admin'>Gerenciar</NavLink></li>
-						<li className='hide-on-large-only'><NavLink to='/carrinho'>Meu Carrinho</NavLink></li>
-						<li className='hide-on-large-only'><NavLink to='/perfil'>Meu Perfil</NavLink></li>
-						<NavItem className='hide-on-large-only' onClick={ (e) => { this.props.handleLogin(null,null,true) } } >Sair</NavItem>
-						<NavItem className='hide-on-large-only' divider/>
-						<li><NavLink to='/'>Página Inicial</NavLink></li>
-						<li><NavLink to='/produtos'>Produtos</NavLink></li>
-						<li><NavLink to='/servicos'>Serviços</NavLink></li>
-					</Navbar>
-				</div> 
-			);
-		} else if(this.props.user) {
-			return(
-				<div>
-					<Navbar className='navbar' brand={<ResponsiveLogo/>} right>
-						<SideNavItem className='hide-on-large-only' userView user={this.props.user}/>
-						<li className='hide-on-large-only'><NavLink to='/carrinho'>Meu Carrinho</NavLink></li>
-						<li className='hide-on-large-only'><NavLink to='/perfil'>Meu Perfil</NavLink></li>
-						<NavItem className='hide-on-large-only' onClick={ (e) => { this.props.handleLogin(null,null,true) } } >Sair</NavItem>
-						<NavItem className='hide-on-large-only' divider/>
-						<li><NavLink to='/'>Página Inicial</NavLink></li>
-						<li><NavLink to='/produtos'>Produtos</NavLink></li>
-						<li><NavLink to='/servicos'>Serviços</NavLink></li>
-					</Navbar>
-				</div>
-			);
+  render() {
+    if(this.props.user && this.props.user.username == 'admin'){
+      return(
+        <div>
+          <Navbar className='navbar' brand={<ResponsiveLogo/>} right>
+            <SideNavItem className='hide-on-large-only' userView user={this.props.user}/>
+            <li><NavLink to='/admin'>Gerenciar</NavLink></li>
+            <li className='hide-on-large-only'><NavLink to='/carrinho'>Meu Carrinho</NavLink></li>
+            <li className='hide-on-large-only'><NavLink to='/perfil'>Meu Perfil</NavLink></li>
+            <NavItem className='hide-on-large-only' onClick={ (e) => { this.props.handleLogin(null,null,true) } } >Sair</NavItem>
+            <NavItem className='hide-on-large-only' divider/>
+            <li><NavLink to='/'>Página Inicial</NavLink></li>
+            <li><NavLink to='/produtos'>Produtos</NavLink></li>
+            <li><NavLink to='/servicos'>Serviços</NavLink></li>
+          </Navbar>
+        </div> 
+      );
+    } else if(this.props.user) {
+      return(
+        <div>
+          <Navbar className='navbar' brand={<ResponsiveLogo/>} right>
+            <SideNavItem className='hide-on-large-only' userView user={this.props.user}/>
+            <li className='hide-on-large-only'><NavLink to='/carrinho'>Meu Carrinho</NavLink></li>
+            <li className='hide-on-large-only'><NavLink to='/perfil'>Meu Perfil</NavLink></li>
+            <NavItem className='hide-on-large-only' onClick={ (e) => { this.props.handleLogin(null,null,true) } } >Sair</NavItem>
+            <NavItem className='hide-on-large-only' divider/>
+            <li><NavLink to='/'>Página Inicial</NavLink></li>
+            <li><NavLink to='/produtos'>Produtos</NavLink></li>
+            <li><NavLink to='/servicos'>Serviços</NavLink></li>
+          </Navbar>
+        </div>
+      );
 
-		} else {
-			return(
-				<div>
-					<Navbar className='navbar' brand={<ResponsiveLogo/>} right>
-						<li><NavLink to='/login' className='hide-on-large-only'>Fazer Login</NavLink></li>
-						<NavItem className='hide-on-large-only' divider/>
-						<li><NavLink to='/' >Página Inicial</NavLink></li>
-						<li><NavLink to='/produtos' >Produtos</NavLink></li>
-						<li><NavLink to='/servicos' >Serviços</NavLink></li>
-					</Navbar>
-				</div>
-			);
+    } else {
+      return(
+        <div>
+          <Navbar className='navbar' brand={<ResponsiveLogo/>} right>
+            <li><NavLink to='/login' className='hide-on-large-only'>Fazer Login</NavLink></li>
+            <NavItem className='hide-on-large-only' divider/>
+            <li><NavLink to='/' >Página Inicial</NavLink></li>
+            <li><NavLink to='/produtos' >Produtos</NavLink></li>
+            <li><NavLink to='/servicos' >Serviços</NavLink></li>
+          </Navbar>
+        </div>
+      );
 
-		}
+    }
 
 
-		
-	}
+    
+  }
 }
 
 class Header extends Component {
 
-	constructor(props) {
-		super(props)
-		this.props = {
-			user: props.user,
-			handleLogin: props.handleLogin
-		};
-	}
+  constructor(props) {
+    super(props)
+    this.props = {
+      user: props.user,
+      handleLogin: props.handleLogin
+    };
+  }
 
-	render() {
-		return(
-			<div id='header'>
-				<TopHeader user={this.props.user} handleLogin={this.props.handleLogin}/>
-				<TopNavbar user={this.props.user} handleLogin={this.props.handleLogin}/>
-			</div>
-		);
-	}
+  render() {
+    return(
+      <div id='header'>
+        <TopHeader user={this.props.user} handleLogin={this.props.handleLogin}/>
+        <TopNavbar user={this.props.user} handleLogin={this.props.handleLogin}/>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- petshop/src/js/userProfile.js
+++ petshop/src/js/userProfile.js
@@ -9,9 +9,10 @@
 import React, { Component } from 'react'
 import { Tabs, Tab } from 'react-materialize';
 import { Row, Col } from 'react-materialize';
-import { Button } from 'react-materialize';
+import { Input, Button } from 'react-materialize';
 
 import EditableTable, { AnimalTable, AddressessTable } from './editableTable'
+import { getFromSessionStorage, getFromLocalStorage } from './mockDB'
 
 import '../css/general.css';
 import '../css/tabs.css';
@@ -19,117 +20,159 @@ import '../css/userProfile.css'
 
 class Configs extends Component {
 
-	validateForm = () => {
-		console.log('submitting')
-		return true
-	}
+  constructor(props) {
+    super(props)
+    this.props = {
+      handleLogin: props.handleLogin
+    }
 
-	render() {
-		return(
-			<div class='container'>
-				<div className='align-content'>
-					<h1 className='label'>Usuário e Senha</h1>
-				</div>
+    let currentUser = getFromSessionStorage("user")
 
-				<div className='scaler align-content'>
-					<img src='https://www.w3schools.com/howto/img_avatar2.png'
-						className='profile-pic' />
+    this.state = {
+      username: currentUser.username,
+      old_pswd: currentUser.password,
+      new_pswd: '',
+      conf_pswd: ''
+    }
+  }
 
-					<div style={{display: 'inline-block', marginBottom: '30px'}}>
-						<h4 className='label'>Senha atual</h4>
-						<form>
-							<Col>
-								<Row>
-									<input className='settings_input box-shadow' 
-												type='password' 
-												placeholder='Senha Atual' 
-												name='oldpassword'
-												required/>
-								</Row>
-								<Row>
-									<p className='text' style={{fontSize: '10px'}}> Sua senha atual é necessária para efetuar qualquer mudança</p>
-								</Row>
-								<Row>
-									<h4 className='label'>Altere sua Senha</h4>
-								</Row>
-								<Row>
-									<input className='settings_input box-shadow' 
-												type='password' 
-												placeholder='Nova Senha' 
-												name='newpassword'
-												required/>
-								</Row>
-								<Row>
-									<input className='settings_input box-shadow' 
-												type='password' 
-												placeholder='Confirmar Senha' 
-												name='confpassword'
-												required/>
-								</Row>
-								<Row>
-									<Button waves='light' className='btn'>Confirmar Alterações</Button>
-								</Row>
-							</Col>
-						</form>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  // Function to check if inputs are correct
+  validate = (e) => {
+    console.log('submitting')
+    return true
+  }
+
+  // Update props value
+  handleChange = (e, stateToChange) => {
+    let newState = {}
+    newState[stateToChange] = e.target.value
+    this.setState(newState)
+  }
+
+  // Used primarily for ENTER press submission
+  handleKeyPress = (e) => {
+
+    if(e.key === 'Enter') {
+      if(this.validate(e)) {
+        this.submit(e)
+      }
+    }
+  }
+
+  submit = (e) => {
+    if(this.validate(e)) 
+      this.props.handleLogin(this.state.username, this.state.password, false)
+  }
+
+  render() {
+    return(
+      <div class='container'>
+        <div className='align-content'>
+          <h1 className='label'>Usuário e Senha</h1>
+        </div>
+
+        <div className='scaler align-content'>
+          <img src='https://www.w3schools.com/howto/img_avatar2.png'
+            className='profile-pic' />
+
+          <div style={{display: 'inline-block', marginBottom: '30px'}}>
+            <h4 className='label'>Senha atual</h4>
+            <Col>
+              <Row>
+                <Input className='settings_input box-shadow' 
+                      type='password' 
+                      placeholder='Senha Atual' 
+                      name='oldpassword'
+                      onChange={e => this.handleChange(e, "old_pswd")}
+                      required/>
+              </Row>
+              <Row>
+                <p className='text' style={{fontSize: '10px'}}> Sua senha atual é necessária para efetuar qualquer mudança</p>
+              </Row>
+              <Row>
+                <h4 className='label'>Altere sua Senha</h4>
+              </Row>
+              <Row>
+                <Input className='settings_input box-shadow' 
+                      type='password' 
+                      placeholder='Nova Senha' 
+                      name='newpassword'
+                      onChange={e => this.handleChange(e, "new_pswd")}
+                      required/>
+              </Row>
+              <Row>
+                <Input className='settings_input box-shadow' 
+                      type='password' 
+                      placeholder='Confirmar Senha' 
+                      name='confpassword'
+                      onChange={e => this.handleChange(e, "conf_pswd")}
+                      required/>
+              </Row>
+              <Row>
+                <Button waves='light' 
+                      className='btn'
+                      onClick={this.changePassword}>Confirmar Alterações</Button>
+              </Row>
+            </Col>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-				
+        
 class Animals extends Component {
   render() {
     return (
       <div>
         <AnimalTable header={[
-					{ name: 'Nome', prop: 'name', type: 'text', editable: true },
-					{ name: 'Raça', prop: 'race', type: 'text', editable: true },
-					{ name: 'Agendamentos', prop: 'scheduled', type: 'text', editable: true },
-					{ name: 'Custo', prop: 'cost', type: 'number', editable: false }
-				]}/>
+          { name: 'Nome', prop: 'name', type: 'text', editable: true },
+          { name: 'Raça', prop: 'race', type: 'text', editable: true },
+          { name: 'Agendamentos', prop: 'scheduled', type: 'text', editable: true },
+          { name: 'Custo', prop: 'cost', type: 'number', editable: false }
+        ]}/>
       </div>
     );
   }
 }
 
 class Addressess extends Component {
-	render() {
+  render() {
     return (
       <div>
         <AddressessTable header={[
-					{ name: 'Nome', prop: 'nickname', type: 'text', editable: true },
-					{ name: 'Rua', prop: 'street', type: 'text', editable: true },
-					{ name: 'N°', prop: 'number', type: 'number', editable: true },
-					{ name: 'Complemento', prop: 'compl', type: 'text', editable: true }
-				]}/>
+          { name: 'Nome', prop: 'nickname', type: 'text', editable: true },
+          { name: 'Rua', prop: 'street', type: 'text', editable: true },
+          { name: 'N°', prop: 'number', type: 'number', editable: true },
+          { name: 'Complemento', prop: 'compl', type: 'text', editable: true }
+        ]}/>
       </div>
     );
   }
 }
 
 class PerfilUsuario extends Component {
-	render() {
-		return(
-			<div className='container' style={{marginTop: '15px'}}>
-				<Tabs className='z-depth-1'>
-					<Tab title='Configurações' className='tab' active>
-						<Configs />
-					</Tab>
+  render() {
+    return(
+      <div className='container' style={{marginTop: '15px'}}>
+        <Tabs className='z-depth-1'>
+          <Tab title='Configurações' className='tab' active>
+            <Configs />
+          </Tab>
 
-					<Tab title='Animais' className='tab'>
-						<Animals />
-					</Tab>
+          <Tab title='Animais' className='tab'>
+            <Animals />
+          </Tab>
 
-					<Tab title='Endereços' className='tab'>
-						<Addressess />
-					</Tab>
-				</Tabs>
-			</div>
-		);
-	}
+          <Tab title='Endereços' className='tab'>
+            <Addressess />
+          </Tab>
+        </Tabs>
+      </div>
+    );
+  }
 }
-			
+      
 
 export default PerfilUsuario;
