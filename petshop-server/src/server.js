d21051de799e30a24f20a2fa0b201020fbcff7bd
Giovanna Guimarães <giovannaguimaraes44@gmail.com>
2018-07-07 04:34:46 +0000

    DELETE implementation

--- petshop-server/src/server.js
+++ petshop-server/src/server.js
@@ -10,10 +10,10 @@
 // Tutorial: https://www.tutorialspoint.com/nodejs/nodejs_restful_api.htm
 // RESTful API
 // GET − Provides a read only access to a resource.
-// PUT − Used to create a new resource.
+// OPTIONS − Used to create a new resource.
 // DELETE − Used to remove a resource.
 // POST − Used to update an existing resource or create a new resource.
-// OPTIONS − Used to get the supported operations on a resource.
+// PUT − Used to get the supported operations on a resource.
 
 var print = console.log
 
